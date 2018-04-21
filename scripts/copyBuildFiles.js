8716fabb2d8a3035b35ac9ea700fd20132810373
Alfredo Perez <alfredo.perez.q@gmail.com>
2018-04-21 15:48:07 -0500

    build - add copy files script

--- scripts/copyBuildFiles.js
+++ scripts/copyBuildFiles.js
@@ -49,7 +49,7 @@ function createPackageFile() {
       } = packageData;
 
       const minimalPackage = {
-        name: "ps-react",
+        name: "reactor-alfredop13",
         author,
         version,
         description,
